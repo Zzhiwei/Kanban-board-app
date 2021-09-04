bce73f05d7ccddeb5f9493544ee0387833a23d12
softmagnet <zhiwei3141@gmail.com>
2021-09-04 19:31:57 +0800

    Add drag functionality

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import '../css/Task.css'
 
 export default function Task({ description }) {
+  const draggableRef = useRef()
+
+  useEffect(() => {
+    const draggable = draggableRef.current
+    draggable.addEventListener('dragstart', e => {
+      draggable.classList.add('dragging')
+    })
+
+    draggable.addEventListener('dragend', e => {
+      draggable.classList.remove('dragging')
+    })
+  }, [])
+
+
   return (
-    <div className="task">
+    <div ref={draggableRef} className="task draggable" draggable="true">
       {description}
     </div>
   )
--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import Task from '../components/Task'
 import '../css/App.css'
 
+function getDragAfterElement(container, y) {
+  const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+  return draggableElements.reduceRight((closest, child) => {
+    const box = child.getBoundingClientRect()
+    const offset = y - box.top - box.height / 2
+    if (offset < 0 ) {
+      return { offset: offset, element: child }
+    } else {
+      return closest
+    }
+  }, { offset: Number.NEGATIVE_INFINITY }).element
+}
+
 export default function Board() {
+  
+  
+  useEffect(() => {
+    const containers = document.querySelectorAll('.board__draggable-container')
+    containers.forEach(container => {
+      container.addEventListener('dragover', e => {
+        e.preventDefault()
+        const isAfterElement = getDragAfterElement(container, e.clientY)
+        const draggable = document.querySelector('.dragging')
+        if (isAfterElement == null) {
+          container.appendChild(draggable)
+        } else {
+          container.insertBefore(draggable, isAfterElement)
+        }
+      })
+    })
+
+  }, [])
+
+
   return (
     <main className="board">
       <div className="max-width">
@@ -14,17 +48,27 @@ export default function Board() {
               <h1 className="board__column-title">
                 Todo
               </h1>
-              <Task description="Eat sleep code repeat" />
+              <div className="board__draggable-container">
+                <Task description="Eat sleep code repeat" />
+                <Task description="Eat sleep code repeat" />
+
+              </div>
             </div>
             <div className="board__column board__in-progress">
               <h1 className="board__column-title">
                 In progress
               </h1>
+              <div className="board__draggable-container">
+                <Task description="Eat sleep code repeat" />
+              </div>
             </div>
             <div className="board__column board__done">
               <h1 className="board__column-title">
                 Done
               </h1>
+              <div className="board__draggable-container">
+                <Task description="Eat sleep code repeat" />
+              </div>
             </div>
           </main>
 
