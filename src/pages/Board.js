e46e6633bf4396ba60de9b2780435b6a39ffd0d2
softmagnet <zhiwei3141@gmail.com>
2021-09-10 18:24:04 +0800

    Improve code quality and add useCallback

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,35 +1,60 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useCallback } from 'react'
 import Task from '../components/Task'
 import '../css/App.css'
 
-function getDragAfterElement(container, y) {
+
+function getDragAfterElement(container, dropYCoordinate) {
   const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
 
-  return draggableElements.reduceRight((closest, child) => {
-    const box = child.getBoundingClientRect()
-    const offset = y - box.top - box.height / 2
-    if (offset < 0 ) {
-      return { offset: offset, element: child }
+  const taskToPlaceAboveOf = draggableElements.reduceRight((closest, taskBox) => {
+    const box = taskBox.getBoundingClientRect()
+    const offsetBtwTaskAndDrop = dropYCoordinate - box.top - box.height / 2
+    if (offsetBtwTaskAndDrop < 0 ) {
+      //if drop is above element, return the element
+      return taskBox
     } else {
       return closest
     }
-  }, { offset: Number.NEGATIVE_INFINITY }).element
+  }, null)
+  return taskToPlaceAboveOf
 }
 
+/*
+I am using vanilla js to do the drag N drop entirely,
+
+If i were to do it with react, I would keep a state of what each container has,
+then at drop event change state causing rerender.
+*/
+
 export default function Board() {
+
+  const renderTodoTasks = useCallback(() => {
+    console.log("rendering todo tasks")
+  },[])
+
+  const renderDoneTasks = useCallback(() => {
+    console.log("rendering done tasks")
+  },[])
+
+  const renderInProgressTasks = useCallback(() => {
+    console.log("rendering in progress tasks")
+  },[])
   
+
   
   useEffect(() => {
     const containers = document.querySelectorAll('.board__draggable-container')
     containers.forEach(container => {
       container.addEventListener('dragover', e => {
         e.preventDefault()
-        const isAfterElement = getDragAfterElement(container, e.clientY)
+        const dropYCoordiante = e.clientY;
+        const taskToPlaceAboveOf = getDragAfterElement(container, dropYCoordiante)
+        const isAboveSomeTask = taskToPlaceAboveOf != null
         const draggable = document.querySelector('.dragging')
-        if (isAfterElement == null) {
+        if (!isAboveSomeTask) {
           container.appendChild(draggable)
         } else {
-          container.insertBefore(draggable, isAfterElement)
+          container.insertBefore(draggable, taskToPlaceAboveOf)
         }
       })
     })
@@ -43,14 +68,17 @@ export default function Board() {
           <header className="heading board__heading">
           ~ Your board name ~
           </header>
-          <main className="board__columns">
+          <div className="board__columns">
             <div className="board__column board__todo">
               <h1 className="board__column-title">
                 Todo
               </h1>
               <div className="board__draggable-container">
-                <Task description="Eat sleep code repeat" />
-                <Task description="Eat sleep code repeat" />
+                {
+                  renderTodoTasks()
+                }
+                <Task description="1" />
+                <Task description="2" />
 
               </div>
             </div>
@@ -59,18 +87,24 @@ export default function Board() {
                 In progress
               </h1>
               <div className="board__draggable-container">
-                <Task description="Eat sleep code repeat" />
+                {
+                  renderInProgressTasks()
+                }
+                <Task description="3" />
               </div>
             </div>
             <div className="board__column board__done">
               <h1 className="board__column-title">
+              {
+                  renderDoneTasks()
+                }
                 Done
               </h1>
               <div className="board__draggable-container">
-                <Task description="Eat sleep code repeat" />
+                <Task description="4" />
               </div>
             </div>
-          </main>
+          </div>
 
       </div>
     </main> 
